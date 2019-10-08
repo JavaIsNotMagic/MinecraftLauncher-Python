92572ff1211bf2c329fbda8076289ce8f656421d
scoutchorton <scoutchorton@yahoo.com>
2019-10-08 18:17:55 -0400

    testing/decode_versions.py is working; updated .gitignore decode_versions uses a regex and is iterated through thanks to the amazing re.finditer() function. I learned how the .gitignore syntax works a little bit. All __pycache__ folders won't be comitted. All data under /cli/downloads will not be comitted. As of the state of the code, the mc folder will remain on commit.

--- cli/testing/decode_versions.py
+++ cli/testing/decode_versions.py
@@ -1,8 +1,7 @@
 import sys,re
-file1 = open(sys.argv[1], "r")
-file = file1.readlines()
-for line in file:
-	a = re.compile(r'(?P<releaseJSON>{\"id\": \"(?P<id>(?:\w|\.|-| )*)\", \"type\": \"(?P<type>\w*)\", \"url\": \"(?P<url>https?://\w*\.\w*\.\w*(?:/(?:\w|\.|-)*)*\.\w*)\", \"time\": \"(?P<time>\d{4}-\d{1,2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2})\", \"releaseTime\": \"(?P<releaseTime>\d{4}-\d{1,2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2})\"})')
-	match = re.search(a, line)
-	print(match.groupdict()['id'] + " " + match.groupdict()['type'])
-#end
+fd = open(sys.argv[1], "r")	#File Descriptor object from the command line
+fo = fd.read()	#File Output as a string of text read from the file specified
+fd.close()	#Close the file
+rePattern = re.compile(r'(?P<releaseJSON>{\"id\": \"(?P<id>(?:\w|\.|-| )*)\", \"type\": \"(?P<type>\w*)\", \"url\": \"(?P<url>https?://\w*\.\w*\.\w*(?:/(?:\w|\.|-)*)*\.\w*)\", \"time\": \"(?P<time>\d{4}-\d{1,2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2})\", \"releaseTime\": \"(?P<releaseTime>\d{4}-\d{1,2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2})\"})')	#Regex to parse URL's
+for i in re.finditer(rePattern, fo):	#For every found case in the JSON file:
+	print(f"\nData:\n\tRelease: {i['id']}\n\tType of release: {i['type']}\n\tURL for release data: {i['url']}\n\tTime?: {i['time']}\n\tRelease time: {i['releaseTime']}")	#Show data for each release (as an example)
