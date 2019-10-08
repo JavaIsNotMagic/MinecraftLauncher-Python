51db80e81460122d9a93dee75d18ed2caeb629db
Connor Tozer <connor.tozer2017@gmail.com>
2019-10-08 19:04:21 -0400

    Create setup.py

--- cli/setup.py
+++ cli/setup.py
@@ -1,9 +1,11 @@
 import pip
 packages_to_install=[]
-for i in packages_to_install:
-	if hasattr(pip, 'main'):
-		pip.main(['install', i])
-	else:
-		pip._internal.main(['install', i])
+if not packages_to_install == '[]':
+	for i in packages_to_install:
+		if hasattr(pip, 'main'):
+			pip.main(['install', i])
+		else:
+			pip._internal.main(['install', i])
+		#end
 	#end
 #end
