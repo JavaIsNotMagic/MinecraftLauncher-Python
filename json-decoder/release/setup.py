087d7ccc7ba640029c99ca3c1734ba0151ef626a
Connor Tozer <connor.tozer2017@gmail.com>
2019-10-13 16:56:38 -0400

    Setup: Move setup files to release

---
+++ json-decoder/release/setup.py
@@ -0,0 +1,18 @@
+import time, subprocess, os, sys
+def PackageInstall(error):
+	lib = str(error)[15:].replace('\'', '')
+	print('>>>',str(error))
+	print('>>> Download will start after five seconds')
+	time.sleep(5)
+	packages_to_install=open(str(os.getcwd() + "/requirements.txt")).readlines()
+	for line in packages_to_install:
+		lib = line
+		subprocess.call("pip3 install " + lib)
+#end
+try:
+	import pygame
+	import nacl.pwhash
+except ImportError as error:
+	PackageInstall(error)
+	pass
+#end
