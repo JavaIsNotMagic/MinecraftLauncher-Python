7cd7026cbcfeaeea5c4d43de874be93e267f5fb5
Connor Tozer <connor.tozer2017@gmail.com>
2019-10-13 21:15:11 -0400

    Add Kivy

---
+++ json-decoder/gui/setup-kivy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#KIVY Installer
+
+import pip,os
+
+pip.main(['install', 'setuptools', virtualenv])
+os.system('python3 -m virtualenv ~/kivy_env')
+os.system('~/kivy_venv/bin/activiate')
+pip.main(['install', 'kivy', 'kivy_examples', 'ffpyplayer'])
