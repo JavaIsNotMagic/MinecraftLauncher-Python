a19471d7ac036fd9933488af5cdeb0d11d9c9902
Connor Tozer <connor.tozer2017@gmail.com>
2019-10-24 14:35:59 -0400

    GUI: Complete basic functuions

--- gui/glade/load-menu.py
+++ gui/glade/load-menu.py
@@ -1,11 +1,16 @@
 import gi,os,sys
+#Other Imports
+path = os.getcwd()
+file = path + "/data/" + "database.db"
+text = None
+text1 = None
 #GUI
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 #Libraries
 lib_path = str(os.getcwd()) + "/libs/glade"
 sys.path.append(lib_path)
-from globals import handler
+from globals import handler, about_menu, help_handler, new_user_handler
 #Main
 builder = Gtk.Builder()
 builder.add_from_file(path + "/glade-menus/" + "menu.glade")
