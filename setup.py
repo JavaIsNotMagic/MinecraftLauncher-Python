4096134bdf83572dc31cccc141e532c8a31f4cb9
Connor Tozer <connor.tozer2017@gmail.com>
2019-11-28 17:50:15 -0500

    Create setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
-import setuptools
+import setuptools,os,platform
+def reqs():
+    windows = open(str(os.getcwd()) + "/req-windows.txt")
+    other = open(str(os.getcwd()) + "/req-other.txt")
+    if platform.system() == 'Windows':
+        return windows.readlines()
+    else:
+        return other.readlines()
+#end
+
+# packages=setuptools.find_packages(),
 setuptools.setup(
     name="mcpy",
-    version="0.1.0",
+    version="0.0.1",
     author="Connor Tozer",
     author_email="connor.tozer2017@gmail.com",
     long_description="mc-python is a minecraft launcher written entirley in python. This launcher supports all vanilla versions.",
     description="A Minecraft Launcher written in Python",
     url="https://github.com/JavaIsNotMagic/MinecraftLauncher-Python",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    #In order to streamline the install, include the requirements.txt
+    install_requires=reqs(),
     python_requires='>=3.6',
 )
