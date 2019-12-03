938f65ffebd95a407de1bdabed70fca566940bbb
scoutchorton <scoutchorton@yahoo.com>
2019-12-03 18:48:54 -0500

    Reduced code for downloading URLs of Forge versions

--- cli/libs/forge.py
+++ cli/libs/forge.py
@@ -18,41 +18,48 @@ def getForgeVersions(version):
 		final = str(os.getcwd()) + "/forge_versions" + version + ".txt"
 	#download the file
 	ur(url, store_decode)
-	f = open(store_decode, "r")
-	for line in f.readlines():
-		if "(Direct Download)" in line:
-			if "universal" in line:
-				#print(line) #debug
-				with open(store, "a+") as r:
-					r.write(re.sub('\s+',' ', line))
-					r.write('\n')
-					r.flush()
-					r.close()
-
-				#end
-			#end
-		else:
-			pass
-		#end
-	#end
-	lines = []
-	with open(store, "r") as t:
-		for line in t.readlines():
-			if "<a href=" in line:
-				#print(line.replace(">", "").replace("<", "").replace("br", "").replace("a href=", "").replace(r'"', "").replace(r"(Direct Download)", "").replace(r"/a", "")) #debug
-				lines.append(line.replace(">","").replace("<","").replace("br","").replace("a href=","").replace(r'"',"").replace(r"(Direct Download)","").replace(r"/a",""))
-		#end
-		t.flush()
-		t.close
-	#end
-	with open(final, "a+") as y:
-		for x in lines:
-			url_full = url_base + x
-			y.write(re.sub('\s+','', url_full))
-			y.write('\n')
-			y.flush()
-		#end
-		y.close()
+	f = open(store_decode, "r")	#Opens the raw HTML file
+	rawHTML = f.read()	#Reads content of the file
+	f.close()	#Closes the file
+	decodeRE = re.finditer(r"<a href=\"(?P<URL>.+)\">\s+<i class=\".+\"></i>\s+Universal", rawHTML)	#Examine regex here: https://regex101.com/r/VnjD9o/1/
+	with open(final, "w") as finalFile:	#Opens the final URL file
+		for match in decodeRE:	#Goes through every match
+			finalFile.write(match.groupdict()["URL"] + "\n")	#Write changes to URL file
+			#print(match.groupdict()["URL"])	#Debug
+		finalFile.close()	#Close URL file
+	#for line in f.readlines():
+	#	if "(Direct Download)" in line:
+	#		if "universal" in line:
+	#			#print(line) #debug
+	#			with open(store, "w") as r:
+	#				r.write(re.sub('\s+',' ', line))
+	#				r.write('\n')
+	#				r.flush()
+	#				r.close()
+	#			#end
+	#		#end
+	#	else:
+	#		pass
+	#	#end
+	##end
+	#lines = []
+	#with open(store, "r") as t:
+	#	for line in t.readlines():
+	#		if "<a href=" in line:
+	#			#print(line.replace(">", "").replace("<", "").replace("br", "").replace("a href=", "").replace(r'"', "").replace(r"(Direct Download)", "").replace(r"/a", "")) #debug
+	#			lines.append(line.replace(">","").replace("<","").replace("br","").replace("a href=","").replace(r'"',"").replace(r"(Direct Download)","").replace(r"/a",""))
+	#	#end
+	#	t.flush()
+	#	t.close
+	##end
+	#with open(final, "w") as y:
+	#	for x in lines:
+	#		url_full = url_base + x
+	#		y.write(re.sub('\s+','', url_full))
+	#		y.write('\n')
+	#		y.flush()
+	#	#end
+	#	y.close()
 	#end
 	print(f"Got Forge versions for {version}!")
 #end
